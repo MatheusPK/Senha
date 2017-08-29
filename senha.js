02fec82a406d4dc937936ad4550a9671ddabc0cb
MatheusPK <matheuskulick@hotmail.com>
2017-08-29 14:23:24 -0300

    cogido base

---
+++ senha.js
@@ -0,0 +1,13 @@
+var fazTabela = function(){
+  var aSecreto = "<tr class = 'codigo'><td><div></div></td><td><div></div></td><td><div></div></td><td><div></div></td></tr>";
+  $(".tabela").append(aSecreto);
+  for (var i = 1; i <= 10; i++) {
+      var aLivre = "<tr id='" + i +"'><td><div class='tentativa'></div></td><td><div class='tentativa'></div></td><td><div class='tentativa'></div></td><td><div class='tentativa'></div></td><div class='tentativa'></div></td></tr>";
+      $(".tabela").append(aLivre);
+      $("tr:last").addClass("tentativa");
+  }
+}
+
+var geraCodigo = function(){
+  var senha = []
+}
