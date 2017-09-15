c333b4cee933b3fa87832c047beb9ec6952499a5
MatheusPK <matheuskulick@hotmail.com>
2017-09-14 22:44:45 -0300

    conserta bugs

--- senha.js
+++ senha.js
@@ -1,11 +1,11 @@
 var fazTabela = function() {
   var aSecreto = "<tr class = 'codigo'><td><div></div></td><td><div></div></td><td><div></div></td><td><div></div></td></tr>";
   $(".tabela").append(aSecreto);
-  for (var i = 1; i <= 10; i++) {
-      var aLivre = "<tr id='" + i +"' class = 'tentativa'><td><div></div></td><td><div></div></td><td><div></div></td><td><div></div></td><div></div></td><td class = 'dica'><div></div><div></div><div></div><div></div></tr>";
+  for (var i = 10; i >= 1; i--) {
+      var aLivre = "<tr id='" + i +"' class = 'tentativa'><td><div></div></td><td><div></div></td><td><div></div></td><td><div></div></td><div></div></td><td id = 'dica' class = '" + i + "'><div></div><div></div><div></div><div></div></tr>";
       $(".tabela").append(aLivre);
   }
-  var selecao = "<tr class = 'selecao'><td><div onclick = 'mudaCor()'></div></td><td><div></div></td><td><div></div></td><td><div></div></td></tr>";
+  var selecao = "<tr class = 'selecao'><td><div></div></td><td><div></div></td><td><div></div></td><td><div></div></td></tr>";
   $(".tabelaS").append(selecao);
 }
 
@@ -17,41 +17,49 @@ var geraCodigo = function(cores) {
   return codigo;
 }
 
-var marcarCores = function(codigo) {
+var marcarCores = function(local, codigo) {
     codigo.forEach(function(cor, indice){
-      $(".codigo td").eq(indice).children().css("background", cor);
+      $(local).eq(indice).css("background", cor);
     })
 }
 
-function compara(codigo, tentativa) {
+var compara = function(codigo, tentativa) {
   var dicas = [];
   for (var i in tentativa) {
-    if (codigo.indexOf(tentativa[i]) == -1) dicas.push('beige');
-    if (tentativa[i] == codigo[i]) dicas.push('black');
-    else dicas.push('white');
+    if (codigo.indexOf(tentativa[i]) == -1) dicas.push('rgb(100, 149, 237)');
+    else if (tentativa[i] == codigo[i]) dicas.push('rgb(0, 0, 0)');
+    else dicas.push('rgb(255, 255, 255)');
   }
   return dicas;
 }
 
-
-var mudaCor = function(){
-  var cores = ["red", "blue", "yellow", "purple", "green", "orange", "pink", "grey"];
-  $(this).css("background", "blue");
-}
-
 var confirma = function(){
+  contador++;
   var tela = [];
   for(var i = 0; i <= 3;i++){
-    tela.push($('.selecao td').children().css("background-color"));
+    tela.push($('.selecao div').eq(i).css("background-color"));
+  }
+  marcarCores("#" + contador + " td div", tela);
+  marcarCores("." + contador + " div", compara(senha, tela));
+}
+
+var mudaCor = function(){
+  var cores = ["rgb(255, 0, 0)", "rgb(0, 0, 255)", "rgb(255, 255, 0)", "rgb(128, 0, 128)", "rgb(0, 255, 0)", "rgb(255, 165, 0)", "rgb(255, 192, 203)", "rgb(128, 128, 128)"];
+  $(this).css("background", cores[contadorCores]);
+  contadorCores++;
+  if (contadorCores > 8) {
+    contadorCores = 0;
   }
 }
 
+var contador = 0;
+var contadorCores = 0;
+var cores = ["rgb(255, 0, 0)", "rgb(0, 0, 255)", "rgb(255, 255, 0)", "rgb(128, 0, 128)", "rgb(0, 255, 0)", "rgb(255, 165, 0)", "rgb(255, 192, 203)", "rgb(128, 128, 128)"];
+var senha = geraCodigo(cores);
 
 $(document).ready(function() {
-  var cores = ["red", "blue", "yellow", "purple", "green", "orange", "pink", "grey"];
   fazTabela();
-  var codigo = geraCodigo(cores);
-  marcarCores(codigo);
+  //marcarCores(".codigo td div", senha);
   $(".selecao div").click(mudaCor);
   $("button").click(confirma);
 })
